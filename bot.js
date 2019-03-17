7e84a64197ad631aebd81ff8ef73f990ab90e798
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-17 06:30:17 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('message', function(message) {
     let args = message.content.split(" ").slice(1).join(" ");
     if(message.content.startsWith(prefix + "setname")) {
 		        if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+            if(!args) return message.reply('اكتب الحالة اللي تريدها بليز.');
         client.user.setUsername(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
            msg.delete(5000);
@@ -45,7 +45,7 @@ client.on('message', function(message) {
         });
     } else if(message.content.startsWith(prefix + "st")) {
 		        if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+            if(!args) return message.reply('اكتب الحالة اللي تريدها  بليز.');
         client.user.setGame(args , 'https://twitch.tv/6xlez1');
         message.channel.send(':white_check_mark: Done!').then(msg => {
            msg.delete(5000);
@@ -61,7 +61,7 @@ client.on('message', function(message) {
         });
     } else if(message.content.startsWith(prefix + "wat")) {
 				        if(message.author.id !== myID) return;
-            if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+            if(!args) return message.reply('اكتب الحالة اللي تريدها  بليز.');
         client.user.setActivity(args, {type:'WATCHING'});
         message.channel.send(':white_check_mark: Done!').then(msg => {
            msg.delete(5000);
@@ -71,7 +71,7 @@ client.on('message', function(message) {
 				        if(message.author.id !== myID) return;
         client.user.setAvatar(args);
         message.channel.send(':white_check_mark: Done!').then(msg => {
-                if(!args) return message.reply('اكتب الحالة اللي تريدها.');
+                if(!args) return message.reply('اكتب الحالة اللي تريدها  بليز.');
            msg.delete(5000);
           message.delete(5000);
         });
